ff350e44b3a2eebf8a0f0021a6abcd630ca6fd8b
GamjaPower <sst@eroun.ai>
2024-09-23 12:13:10 +0900

    feat:  sameSite 테스트

--- api/server/services/AuthService.js
+++ api/server/services/AuthService.js
@@ -346,7 +346,8 @@ const setAuthTokens = async (userId, res, sessionId = null) => {
       expires: new Date(refreshTokenExpires),
       httpOnly: true,
       secure: isProduction,
-      sameSite: 'none',
+      // sameSite: 'none',
+      sameSite: 'strict',
     });
 
     return token;
